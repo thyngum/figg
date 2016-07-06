7debb3f4ea7607844108c7c6ae46b414ac1e9d1c
priesgo <priesgoferreiro@gmail.com>
2016-07-06 15:48:46 -0500

    initial commit

---
+++ src/figg/figg_cli_wrapper.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import argparse
+import sys
+import logging
+import figg
+
+
+def main():
+    
+    # configures logging
+    logging.basicConfig(level=logging.INFO)
+        
+    # Parses input parameters
+    parser = argparse.ArgumentParser(
+            description='figg')
+    parser.add_argument('input_file',
+                        help='Input file')
+    parser.add_argument('--circular', dest='circular', action='store_const',
+                        default=False, const=True, 
+                        help='A flag indicating if the genome is circular.')
+    parser.add_argument('--output-format', dest='output_format', action='store', 
+                        required = True, choices=set(["mega", "paup", "phylip", "all"]),
+                        help='The output format')
+    args = self.parser.parse_args()
+    
+    # Read input parameters
+    input_file = args.input_file
+    logging.info("Input file: [%s]" % input_file)
+    is_circular = args.circular
+    logging.info("Circular genomes: [%s]" % str(is_circular))
+    output_format = args.output_format
+    logging.info("Output format: [%s]" % output_format)
+    
+    # Calls the program
+    try:
+        figg.run_figg(input_file, is_circular, output_format)
+        logging.error("Figg finished!")
+    except Exception, e:
+        logging.error("Figg raised an error: [%s]" % str(e))
+    
+    
+if __name__ == '__main__':
+    main()
---
+++ src/figg/figg_core.py
@@ -0,0 +1,11 @@
+
+
+
+
+def run_figg(input_file, is_circular, output_format):
+    """
+    Put your code here!
+    """
+    pass
+
+
