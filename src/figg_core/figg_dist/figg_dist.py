f5a2323c18cdcb9a463b1891ed5d59500af63fa3
Melolab <melo@node1.melolab>
2016-07-13 14:21:03 -0400

    Finish cleaning figg_dist

--- src/figg_core/figg_dist/figg_dist.py
+++ src/figg_core/figg_dist/figg_dist.py
@@ -1,105 +1,109 @@
 import logging
 
 """
-Functions to work with distance matrices
+Functions to compute distance matrices
 """
 
-def AD(matrix_1, matrix_2):
-	"Computes the measure of differences between two adjacency matrices, AD"
+def adj_matrix(order, ref_order):
+	"Computes the adjacency matrix for a given genome"
 
-	d = 0 
+	num_genes = len(set(ref_order))
+	matrix = [[0]*num_genes for i in range(num_genes)]
+	index_list = [ref_order.index(i) if i in ref_order else ref_order.index(i.replace('-','')) for i in order]
+	for i in range(len(index_list)-1):
+		if "-" in list(order[i+1]):
+			matrix[index_list[i]][index_list[i+1]] = -1
+		else:
+			matrix[index_list[i]][index_list[i+1]] = 1 
+
+	return matrix
+
+
+def adj_matrix_set(ref_matrix, ref_order, gene_orders):
+	"Computes the set of all adjacency matrices" 
+
+	num_genomes = len(gene_orders) + 1
+
+	# Initialize the set of adjacency matrices and append the reference 
+	matrix_set = []
+	matrix_set.append(ref_matrix) 
+
+	# Append the matrices for all other genomes 
+	[matrix_set.append(adj_matrix(gene_orders[i], ref_order)) for i in range(len(gene_orders))]
+
+	return matrix_set 
+
+
+def AD(matrix_1, matrix_2):
+	"Counts the differences between two adjacency matrices (AD)"
+
+	diff = 0 
 	for i in range(len(matrix_1)):
 		for j in range(len(matrix_2)):
-			if matrix_1[i][j] == matrix_2[i][j]:
-				d += 1
-	
-	return(d)
+			if ( matrix_1[i][j] != matrix_2[i][j] ):
+				diff += 1
+
+	return diff
 
 
-def workspace(rseq, aseq):
-	n = len(set(rseq))
-	K = [[0]*n for i in range(n)]
+def dist_matrix(adj_matrices):
+	"Computes uncorrected distance matrix (matrix of AD)" 
 
-	ref = [rseq.index(i) if i in rseq else rseq.index(i.replace('-','')) for i in aseq]
-	for i in range(len(ref)-1):
-		if "-" in list(aseq[i+1]):
-			K[ref[i]][ref[i+1]] = -1
-		else:
-			K[ref[i]][ref[i+1]] = 1 
-	return K
-
-
-def dmatrix(ref_string_adjacency_matrix, ref_string, strings):
-	"Computes the uncorrected distance matrix (matrix of AD)" 
-
-	num_genomes = len(strings) + 1
-
-	temp_matrix = []
-	temp_matrix.append(ref_string_adjacency_matrix)
-	[temp_matrix.append(workspace(ref_string, strings[i])) for i in range(len(strings))]
-	distance_matrix = [[0]*num_genomes for i in range(num_genomes)]
+	num_genomes = len(adj_matrices)
+	matrix = [[0]*num_genomes for i in range(num_genomes)]
 	for i in range(num_genomes):
 		for j in range(i):
-			distance_matrix[i][j] = AD(temp_matrix[i], temp_matrix[j])
+			matrix[i][j] = AD(adj_matrices[i], adj_matrices[j])
 			
-	return distance_matrix, temp_matrix 
+	return matrix 
 
 
-def fpos(temp_matrix):
+def freq_matrix_pos(adj_matrices):
 	"Computes the positive frequency matrix"
 
-	n = len(temp_matrix[0])
-	K = [[0]*n for i in range(n)]
-	for i in range(len(K)):
-		for j in range(len(K)):
-			K[i][j] = round(sum([1 for x in range(len(temp_matrix)) if temp_matrix[x][i][j] == 1]) / float(len(temp_matrix)),4)
+	num_genes = len(adj_matrices[0])
+	matrix = [[0]*num_genes for i in range(num_genes)]
+	for i in range(len(matrix)):
+		for j in range(len(matrix)):
+			matrix[i][j] = round(sum([1 for x in range(len(adj_matrices)) if adj_matrices[x][i][j] == 1]) / float(len(adj_matrices)),4)
 	
-	return K
+	return matrix
 
 
-def fneg(temp_matrix):
+def freq_matrix_neg(adj_matrices):
 	"Computes the negative frequency matrix"
 
-	n = len(temp_matrix[0])
-	K = [[0]*n for i in range(n)]
-	for i in range(len(K)):
-		for j in range(len(K)):
-			K[i][j] = round(sum([1 for x in range(len(temp_matrix)) if temp_matrix[x][i][j] == -1]) / float(len(temp_matrix)),4)
+	num_genes = len(adj_matrices[0])
+	matrix = [[0]*num_genes for i in range(num_genes)]
+	for i in range(len(matrix)):
+		for j in range(len(matrix)):
+			matrix[i][j] = round(sum([1 for x in range(len(adj_matrices)) if adj_matrices[x][i][j] == -1]) / float(len(adj_matrices)),4)
 	
-	return K
-
-
-def cdiff(i, j, matrix_1, matrix_2, positive_freqs, negative_freqs):
-	"Similar to diff() but takes into account positive and negative frequencies"
-
-	if matrix_1[i][j] == matrix_2[i][j]:
-		return(0)
-	elif (matrix_1[i][j] == matrix_2[i][j]) & (matrix_1[i][j] > 0 & matrix_2[i][j] > 0):
-		return(1 - positive_freqs[i][j])
-	elif (not(matrix_1[i][j] == matrix_2[i][j]) & (matrix_1[i][j]) < 0 | matrix_2[i][j] < 0):
-		return(1 - negative_freqs[i][j])
+	return matrix
 
 
 def ADc(matrix_1, matrix_2, positive_freqs, negative_freqs):
-	"Computes the corrected measure of differences between two adjacency matrices, ADc"
+	"Similar to AD() but uses frequencies to correct the observed differences"
 
-	dc = 0 
+	diff = 0 
 	for i in range(len(matrix_1)):
 		for j in range(len(matrix_1)):
-			dc += cdiff(i, j, matrix_1, matrix_2, positive_freqs, negative_freqs)
+			if ( matrix_1[i][j] != matrix_2[i][j] ):
+				if ( matrix_1[i][j] > 0 & matrix_2[i][j] > 0 ):
+					diff += (1 - positive_freqs[i][j])
+				else:
+					diff += (1 - negative_freqs[i][j])
 	
-	return(dc)
+	return diff
 
 
-def cdmatrix(temp_matrix):
-	"Computes the corrected distance matrix (matrix of AD)"
+def dist_matrix_corrected(adj_matrices, pos_freqs, neg_freqs):
+	"Computes the corrected distance matrix (matrix of ADc)"
 
-	positive_freqs = fpos(temp_matrix)
-	negative_freqs = fneg(temp_matrix)
-	k = len(temp_matrix)
-	CD =  [[0]*k for i in range(k)]
-	for i in range(k):
+	num_genomes = len(adj_matrices)
+	matrix = [[0]*num_genomes for i in range(num_genomes)]
+	for i in range(num_genomes):
 		for j in range(i):
-			CD[i][j] = ADc(temp_matrix[i],temp_matrix[j], positive_freqs, negative_freqs)
+			matrix[i][j] = ADc(adj_matrices[i],adj_matrices[j], pos_freqs, neg_freqs)
 			
-	return CD, positive_freqs, negative_freqs   
+	return matrix   
